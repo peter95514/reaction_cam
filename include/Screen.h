bb7ad06194c64159b6404790d670aa3537a16035
peter95514 <peter95514@gmail.com>
2026-09-24 13:04:21 +0800

    add font

--- include/Screen.h
+++ include/Screen.h
@@ -5,7 +5,7 @@ enum class ScreenId { Menu, Setting, Trial, Exit };
 class Screen {
 public:
     virtual ~Screen() = default;
-    virtual void enter();
-    virtual void exit();
+    virtual void enter(){};
+    virtual void exit(){};
     virtual ScreenId tick() = 0;
 };
